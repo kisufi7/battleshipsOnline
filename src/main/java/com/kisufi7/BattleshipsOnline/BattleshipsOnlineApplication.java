e342d8b1385e2c862e41c23f18240e43447ad768
kisufi7 <kilian.isufi@gmail.com>
2026-08-30 12:11:22 +0200

    initial commit

---
+++ src/main/java/com/kisufi7/BattleshipsOnline/BattleshipsOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.kisufi7.BattleshipsOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipsOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipsOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kisufi7/BattleshipsOnline/BattleshipsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kisufi7.BattleshipsOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
